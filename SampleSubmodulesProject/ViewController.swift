17a78e02176b3feb1448a37f1dd4c16108d0e649
ben.dang <ben.dang@geocomply.com>
2023-01-18 11:05:42 +0700

    first init

---
+++ SampleSubmodulesProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleSubmodulesProject
+//
+//  Created by Tri.Dang on 18/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
