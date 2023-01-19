b395702066af89d763cbe4923bb1468d3f2f1f4a
ben.dang <ben.dang@geocomply.com>
2023-01-20 02:29:57 +0700

    init module

--- SampleSubmodulesProject/ViewController.swift
+++ SampleSubmodulesProject/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let test = GCVolumeInfo(volumeName: "", volumeDirectory: "")
         // Do any additional setup after loading the view.
     }
 
