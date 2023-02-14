251e85713199ac7ee748b6de5a367193b8cc0143
ben.dang <ben.dang@geocomply.com>
2023-02-14 17:47:08 +0700

    change to static

---
+++ SampleSubmodulesProject/SampleSubmodulesProject/SampleSubmodulesProject.swift
@@ -0,0 +1,10 @@
+//
+//  SampleSubmodulesProject.swift
+//  SampleSubmodulesProject
+//
+//  Created by Tri.Dang on 14/02/2023.
+//
+
+open class SampleSubmodulesProject {
+    var sampleString: String = "test A"
+}
